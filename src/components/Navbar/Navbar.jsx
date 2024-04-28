52a29af3d4d0edf3a25ab157588d0d2625fa3403
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-04-27 21:24:44 -0400

    img and icon added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import "./Navbar.css"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import left from "../../Icons/icons8-left-2-50.png";
+import right from "../../Icons/icons8-right-2-50.png";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className='nav-container'>
-      <div className='navbar'>
-      <NavLink className="nav-home" to="/" >Home</NavLink>
-      <NavLink className="nav-name" to="/">Abdul Rehman</NavLink>
-      <NavLink className="nav-contact" to="/contact" >Contact</NavLink>
+    <div className="nav-container">
+      <div className="navbar">
+        <NavLink className="nav-name" to="/">
+          <img className="icon" src={right} alt="icon" />
+          Abdul Rehman
+          <img className="icon" src={left} alt="icon" />
+        </NavLink>
+        <NavLink className="nav-contact" to="/contact">
+          Contact
+        </NavLink>
+      </div>
     </div>
-    
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import Experience from '../../components/Experience/Experience.jsx'
-import Education from "../../components/Education/Education.jsx"
-import Skills from "../../components/Skills/Skills.jsx"
-import Project from "../../components/Projects/Project.jsx"
-import "./Home.css"
+import React from "react";
+import Experience from "../../components/Experience/Experience.jsx";
+import Education from "../../components/Education/Education.jsx";
+import Skills from "../../components/Skills/Skills.jsx";
+import Project from "../../components/Projects/Project.jsx";
+import abdul from "../../Icons/ar.jpeg";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      <div className='abdul-portfolio'>
-        <img src='./public/Icons/ar.jpeg' alt='' />
-        <h2>Abdul Rehman</h2>
+    <div className="home-container">
+      <div className="abdul-portfolio">
+        <h2 className="home-ar">ᗩᗷᗪᑌᒪ ᖇEᕼᗰᗩᑎ</h2>
+        <p className="summary">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A veniam
+          illum at cum nihil, blanditiis fuga reiciendis deleniti rerum totam
+          natus doloremque molestiae repudiandae nulla sunt quas nesciunt enim
+          laborum.
+        </p>
+        <img className="img-ar" src={abdul} alt="ar" />
       </div>
-      <section>
-        <div>
+      <section className="section-skills">
+        <div className="skills">
           <Skills />
         </div>
       </section>
-      <section>
-        <div>
+      <section className="section-ex">
+        <div className="div-ex">
           <Experience />
         </div>
       </section>
-      <section>
-        <div>
+      <section className="section-edu">
+        <div className="div-edu">
           <Education />
         </div>
       </section>
-      <section>
-        <div>
+      <section className="section-project">
+        <div className="div-project">
           <Project />
         </div>
       </section>
-
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
