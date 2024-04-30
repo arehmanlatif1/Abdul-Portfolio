2a855306685d5e5cbeaf5af4561ac1a23bf4c096
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-04-30 13:15:40 -0400

    skill uder line

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -12,114 +12,120 @@ import "./Skill.css";
 function Skills() {
   return (
     <div className="skills-main">
-
-    <h3 className="tab-header">Skills</h3>
-    <div className="skills-container">
+      <h3 className="tab-header">
+        <span>Skills</span>
+      </h3>
+      <div className="skills-container">
         <div className="skills-slider">
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="HTML" /> HTML
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={styling} alt="CSS" /> CSS
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={java} alt="JavaScript" /> JavaScript
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={pythons} alt="Python" /> Python
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={sql} alt="SQL" /> SQL
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="Node.js" /> Node.js
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={reactjs} alt="React" /> React
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="MongoDB" /> MongoDB
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="Express" /> Express
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="PostgreSQL" /> PostgreSQL
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={django} alt="Django" /> Django
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="JQuery" /> JQuery
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="Bootstrap" /> Bootstrap
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="Semantic UI" /> Semantic UI
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="RESTful Routing" /> RESTful Routing
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="JSON" /> JSON
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="API" /> API
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="Github" /> Github
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="MongoDB" /> MongoDB
-                </p>
-            </div>
-            <div className="skillx" style={{ backgroundColor: '#e8eaf6' }}>
-                <p>
-                    <img className="skills-icon" src={html} alt="PostgreSQL" /> PostgreSQL
-                </p>
-            </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="HTML" /> HTML
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={styling} alt="CSS" /> CSS
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={java} alt="JavaScript" />{" "}
+              JavaScript
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={pythons} alt="Python" /> Python
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={sql} alt="SQL" /> SQL
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="Node.js" /> Node.js
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={reactjs} alt="React" /> React
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="MongoDB" /> MongoDB
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="Express" /> Express
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="PostgreSQL" />{" "}
+              PostgreSQL
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={django} alt="Django" /> Django
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="JQuery" /> JQuery
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="Bootstrap" />{" "}
+              Bootstrap
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="Semantic UI" />{" "}
+              Semantic UI
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="RESTful Routing" />{" "}
+              RESTful Routing
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="JSON" /> JSON
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="API" /> API
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="Github" /> Github
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="MongoDB" /> MongoDB
+            </p>
+          </div>
+          <div className="skillx" style={{ backgroundColor: "#e8eaf6" }}>
+            <p>
+              <img className="skills-icon" src={html} alt="PostgreSQL" />{" "}
+              PostgreSQL
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-
   );
 }
 export default Skills;
