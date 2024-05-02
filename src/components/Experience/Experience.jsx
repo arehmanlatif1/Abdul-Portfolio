de697b863e1273fd4042470563a95e8cfab0a529
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-05-02 00:41:39 -0400

    commit

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,91 +1,60 @@
 import React from "react";
 import usa from "../../Icons/usa.png";
-import babyplanet from "../../Icons/babyplanet.pk.png";
+import developer from "../../gif-files/giphy7.gif"
+import babyplanet from "../../Icons/babyplanet.pkk.png";
 import explore from "../../Icons/explore.png";
 import "./Experience.css";
 
 function Experience() {
   return (
-    <div>
-      <h2 className="title">Experience</h2>
+    <div className="ex-header">
+      <h2 className="title">
+        <span>Experience</span>
+      </h2>
 
       <div className="tab-display">
         <div className="ex-container">
-          <div className="company-name">
+          <div className="company-header">
             <h3 className="ex-company">Explore Logic’s IT Solutions</h3>
-            <img className="explore" src={explore} alt="logics" />
+            <img className="company-logo" src={explore} alt="Explore Logics" />
           </div>
-
-          <h3 className="ex-tab">Web Developer</h3>
+          <h3 className="ex-role">Web Developer</h3>
           <h6 className="ex-date">Feb 2020 - Aug 2022</h6>
-          <p className="ex-p">
-            <ul>
-              <li>
-                Developed and maintained responsive websites using HTML and CSS,
-                focusing on cross-browser compatibility.
-              </li>
-              <li>
-                Ensured optimized performance and user experience across all
-                platforms.
-              </li>
-            </ul>
-          </p>
+          <ul className="ex-details">
+            <li>Developed and maintained responsive websites using HTML and CSS, focusing on cross-browser compatibility.</li>
+            <li>Ensured optimized performance and user experience across all platforms.</li>
+          </ul>
         </div>
 
         <div className="ex-container">
-          <div className="company-name">
-            <h3 className="ex-company">Babyplanet.pk </h3>
-            <img className="explore" src={babyplanet} alt="logics" />
+          <div className="company-header">
+            <h3 className="ex-company">Babyplanet.pk</h3>
+            <img className="company-logo" src={babyplanet} alt="Babyplanet" />
           </div>
-          <h3 className="ex-tab">Customer Service</h3>
-          {/* <h4 className="ex-title">Social Media Management</h4> */}
-
+          <h3 className="ex-role">Customer Service</h3>
           <h6 className="ex-date">Sept 2016 - Feb 2018</h6>
-          <p className="ex-p">
-            <ul>
-              <li>
-                Managed customer service operations and social media content to
-                increase brand visibility and engagement.
-              </li>
-              <li>
-                Developed effective customer engagement strategies to ensure
-                satisfaction.
-              </li>
-            </ul>
-          </p>
+          <ul className="ex-details">
+            <li>Managed customer service operations and social media content to increase brand visibility and engagement.</li>
+            <li>Developed effective customer engagement strategies to ensure satisfaction.</li>
+          </ul>
         </div>
+
         <div className="ex-container">
-          <div className="company-name">
+          <div className="company-header">
             <h3 className="ex-company">University of South Asia</h3>
-            <img className="explore" src={usa} alt="logics" />
+            <img className="company-logo" src={usa} alt="University of South Asia" />
           </div>
-          <h4 className="ex-tab">Social Media Intern</h4>
+          <h3 className="ex-role">Social Media Intern</h3>
           <h6 className="ex-date">Nov 2015 - May 2018</h6>
-          <p className="ex-p">
-            <ul>
-              <li>
-                Engineered and executed marketing strategies, enhancing brand
-                identity and conducting collaborative market research.
-              </li>
-              <li>
-                Boosted brand engagement through targeted social media
-                promotions on platforms like Facebook and Instagram.
-              </li>
-            </ul>
-          </p>
+          <ul className="ex-details">
+            <li>Engineered and executed marketing strategies, enhancing brand identity and conducting collaborative market research.</li>
+            <li>Boosted brand engagement through targeted social media promotions on platforms like Facebook and Instagram.</li>
+          </ul>
         </div>
       </div>
+      <img className="ex-image" src={developer} alt="Developer Experience" />
     </div>
   );
 }
 
 export default Experience;
-
-{
-  /* Assisted in maintaining brand identity by working on marketing
-          campaigns Conducted market research and collaborated with other
-          marketing professionals to create effective campaigns. Boosted and
-          promoted campaigns through social media platforms, including Facebook
-          and Instagram. Contributed to the success of marketing campaigns
-          through strategic planning and content creation  */
-}
