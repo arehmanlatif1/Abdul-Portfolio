4d38af15e7e446aef1f7a51fb37c9b9a62771795
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-05-16 01:53:42 -0400

    skjcnsd

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 
 function Contact() {
-  return (
-    <div className="contact-container">
-      <h2 className="contact-titile">
-        <span>Contact</span>
-      </h2>
-    </div>
-  );
-}
+    return (
+        <div className="contact-container">
+            <h2 className="contact-titile">
+                <span>Contact Me</span>
+            </h2>
+        </div>
 
+    )
+}
 export default Contact;
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -4,10 +4,10 @@ import htmlicon from "../../gif-files/Html.json";
 import styling from "../../gif-files/Cssheet.json";
 import js from "../../gif-files/Javascript.json";
 import github from "../../gif-files/Github.json";
-import ec from "../../gif-files/CurrencyEx.json"
-import cloudy from "../../gif-files/Clouds.json"
-import rektgame from "../../gif-files/Games.json"
-import product from "../../gif-files/Product.json"
+import ec from "../../gif-files/CurrencyEx.json";
+import cloudy from "../../gif-files/Clouds.json";
+import rektgame from "../../gif-files/Games.json";
+import product from "../../gif-files/Product.json";
 import "./Project.css";
 
 function Project() {
@@ -17,19 +17,25 @@ function Project() {
         <span>TECHNICAL PROJECTS</span>
       </h2>
 
-      <div className="project-main"> 
+      <div className="project-main">
         {/* Currency Exchange Project */}
 
         <section className="section-container">
           <div className="app-container">
-          <Lottie
-                  animationData={ec}
-                  loop={true}
-                />
-            <h3 className="app-title">Currency Exchange</h3>
+            <Lottie animationData={ec} loop={true} />
+            <h3 className="app-title">
+              {" "}
+              <a href="https://exchangecurrencies.netlify.app/">
+                
+                " Currency Exchange "
+              </a>
+            </h3>
             <p className="app-description">
               {/* Utilized an API link to fetch the data that convert the currencies with real-time rates for seamless global transactions. */}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi aliquid harum, iure voluptate autem, consequatur, cupiditate animi quam soluta modi aliquam sit recusandae ab cumque quas quis ut nobis dicta!
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
+              aliquid harum, iure voluptate autem, consequatur, cupiditate animi
+              quam soluta modi aliquam sit recusandae ab cumque quas quis ut
+              nobis dicta!
             </p>
 
             <ul className="app-icon">
@@ -81,10 +87,15 @@ function Project() {
         <section className="section-container">
           <div className="app-container">
             <Lottie animationData={cloudy} />
-            <h3 className="app-title">Weather App</h3>
+            <h3 className="app-title">
+              <a href="https://weather-app-caramel.netlify.app/">" Weather App "</a>
+            </h3>
             <p className="app-description">
               {/* Developed a dynamic weather application using HTML, CSS, and JavaScript, fetching real-time weather data from an API. */}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio impedit numquam possimus ipsam, deserunt iusto natus qui consequuntur eum inventore, ipsa quidem? Deleniti sunt ab facere vitae rem aspernatur enim.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio impedit numquam possimus ipsam, deserunt iusto natus
+              qui consequuntur eum inventore, ipsa quidem? Deleniti sunt ab
+              facere vitae rem aspernatur enim.
             </p>
 
             <ul className="app-icon">
@@ -112,6 +123,7 @@ function Project() {
                 />
                 Javascript
               </li>
+              <li>React</li>
             </ul>
           </div>
           {/* <div className="site-link">
@@ -135,12 +147,21 @@ function Project() {
         </section>
 
         <section className="section-container">
-        <div className="app-container">
-            <Lottie animationData={product} loop={true} style={{ width: "15rem", height: "17.5rem" }} />
-            <h3 className="app-title">Product API</h3>
+          <div className="app-container">
+            <Lottie
+              animationData={product}
+              loop={true}
+              style={{ width: "15rem", height: "17.5rem" }}
+            />
+            <h3 className="app-title">
+              <a href="https://product-api-project.netlify.app/">" Product API "</a>
+            </h3>
             <p className="app-description">
               {/* Create a Department store application, Using the  HTML, CSS, REACT and Javascript. */}
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, aliquid earum veniam enim nihil reiciendis labore animi, facilis doloremque nemo nesciunt minus quod inventore, ipsam odit. Eaque quasi molestiae possimus?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Suscipit, aliquid earum veniam enim nihil reiciendis labore animi,
+              facilis doloremque nemo nesciunt minus quod inventore, ipsam odit.
+              Eaque quasi molestiae possimus?
             </p>
 
             <ul className="app-icon">
@@ -168,6 +189,7 @@ function Project() {
                 />
                 Javascript
               </li>
+              <li>React</li>
             </ul>
           </div>
           {/* <div className="site-link">
@@ -188,19 +210,24 @@ function Project() {
             </li>
           </ul>
         </div> */}
-
         </section>
 
         {/* REK'T Project */}
         <section className="section-container">
           <div className="app-container">
             <Lottie animationData={rektgame} />
-            <h3 className="app-title">REK’T</h3>
+            <h3 className="app-title">
+              {" "}
+              <a href="https://rekt-api.netlify.app/">" REK’T "</a>
+            </h3>
             <p className="app-description">
               {/* Spearheaded the development of REK'T, a comprehensive gaming
               review website featuring front-end and back-end functionalities.
               Utilized JSON data for comprehensive game details and images. */}
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam fuga dolore similique, unde optio numquam qui quaerat molestiae natus neque ullam eaque architecto illo quo, perspiciatis laboriosam vitae autem? Similique!
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam
+              fuga dolore similique, unde optio numquam qui quaerat molestiae
+              natus neque ullam eaque architecto illo quo, perspiciatis
+              laboriosam vitae autem? Similique!
             </p>
 
             <ul className="app-icon">
@@ -227,6 +254,10 @@ function Project() {
                   style={{ width: "3.5rem" }}
                 />
                 Javascript
+              </li>
+              <li>
+               
+                React
               </li>
             </ul>
           </div>
@@ -249,14 +280,16 @@ function Project() {
 
         <section className="section-container">
           <div className="app-container">
-          <Lottie
-                  animationData={ec}
-                  loop={true}
-                />
-            <h3 className="app-title">Code.Pal</h3>
+            <Lottie animationData={ec} loop={true} />
+            <h3 className="app-title">
+              <a href="https://exchangecurrencies.netlify.app/">" Code.Pal "</a>
+              </h3>
             <p className="app-description">
               {/* We create a freeleance website, where a client can hire a delveloper */}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cupiditate deserunt quae omnis minus vero nostrum? Necessitatibus saepe, asperiores debitis sit magni eveniet tenetur quo rem harum impedit tempore velit?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores cupiditate deserunt quae omnis minus vero nostrum?
+              Necessitatibus saepe, asperiores debitis sit magni eveniet tenetur
+              quo rem harum impedit tempore velit?
             </p>
 
             <ul className="app-icon">
@@ -303,13 +336,6 @@ function Project() {
           </ul>
         </div> */}
         </section>
-
-
-
-
-
-        
-
       </div>
     </div>
   );
