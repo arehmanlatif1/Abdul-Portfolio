ec6195ce852e77f1860ac4480c17a2a523b652df
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-05-16 17:14:05 -0400

    jknjkcdx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
+import github from "../../gif-files/Github.json";
+import linkedin from "../../gif-files/LinkedIn.json"
+import email from "../../gif-files/Email.json"
+import Lottie from "lottie-react";
 import "./Contact.css";
 
 function Contact() {
@@ -7,6 +11,27 @@ function Contact() {
             <h2 className="contact-titile">
                 <span>Contact Me</span>
             </h2>
+
+            <div className="contact-main">
+                <h3 className="con-class">Keep In Touch!</h3>
+                <p className="con-msg">DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.</p>
+                <i>New York, NY. USA</i>
+                <div className="con-item-div">
+                <ul className="con-items">
+                    <li>
+                    <Lottie animationData={github} loop={true}  style={{ width: "5rem" }} />
+                        Github
+                        </li>
+                    <li>
+                    <Lottie animationData={linkedin} loop={true}  style={{ width: "5rem" }} />
+                        LinkedIn</li>
+                    <li>
+                    <Lottie animationData={email} loop={true}  style={{ width: "7rem" }} />
+                        Email</li>
+                </ul>
+                </div>
+
+            </div>
         </div>
 
     )
