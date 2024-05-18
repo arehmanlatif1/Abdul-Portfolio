02a4beaea7c939b1624fea6a8b00b7f9c152524d
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-05-17 22:25:13 -0400

    skjnbdcs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,67 +1,9 @@
-// import React from "react";
-// import github from "../../gif-files/Github.json";
-// import linkedin from "../../gif-files/LinkedIn.json";
-// import email from "../../gif-files/Email.json";
-// import topbar from "../../gif-files/Top.json";
-// import Lottie from "lottie-react";
-// import "./Contact.css";
-
-// function Contact() {
-//   return (
-//     <div className="contact-container">
-//       <h2 className="contact-titile">
-//         <span>Contact Me</span>
-//       </h2>
-//       <div className="contact-main">
-//         <h3 className="con-class">Keep In Touch!</h3>
-
-//         <p className="con-msg">
-//           DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
-//         </p>
-//         <i>New York, NY. USA</i>
-//         <div className="con-item-div">
-//           <ul className="con-items">
-//             <li>
-//               <a href="https://github.com/arehmanlatif1" className="social-btns">
-//                 <Lottie
-//                   animationData={github}
-//                   loop={true}
-//                   style={{ width: "4rem" }}
-//                 />
-//                 Github
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://www.linkedin.com/in/abdulrehmanlatif1/" className="social-btns">
-//                 <Lottie
-//                   animationData={linkedin}
-//                   loop={true}
-//                   style={{ width: "5rem" }}
-//                 />
-//                 LinkedIn
-//               </a>
-//             </li>
-//             <li>
-//               <a href="mailto:arehmanlatif@gmail.com" className="social-btns">
-//                 <Lottie
-//                   animationData={email}
-//                   loop={true}
-//                   style={{ width: "7rem" }}
-//                 />
-//                 Email
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Contact;
-
-
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import github from "../../gif-files/Github.json";
+import linkedin from "../../gif-files/LinkedIn.json";
+import email from "../../gif-files/Email.json";
+import Lottie from "lottie-react";
 
 
 
@@ -90,7 +32,51 @@ const Contact = () => {
   };
 
   return (
-      <form ref={form} onSubmit={sendEmail}>
+    <div className="contact-container">
+      <h2 className="contact-titile">
+        <span>Contact Me</span>
+      </h2>
+      <div className="contact-main">
+        <h3 className="con-class">Keep In Touch!</h3>
+        <p className="con-msg">
+          DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
+        </p>
+        <i>New York, NY. USA</i>
+        <div className="con-item-div">
+          <ul className="con-items">
+            <li>
+              <a href="https://github.com/arehmanlatif1" className="social-btns">
+                <Lottie
+                  animationData={github}
+                  loop={true}
+                  style={{ width: "4rem" }}
+                />
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/abdulrehmanlatif1/" className="social-btns">
+                <Lottie
+                  animationData={linkedin}
+                  loop={true}
+                  style={{ width: "5rem" }}
+                />
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="mailto:arehmanlatif@gmail.com" className="social-btns">
+                <Lottie
+                  animationData={email}
+                  loop={true}
+                  style={{ width: "7rem" }}
+                />
+                Email
+              </a>
+            </li>
+          </ul>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
         <input type="text" name="user_name" />
         <label>Email</label>
@@ -99,6 +85,9 @@ const Contact = () => {
         <textarea name="message" />
         <input type="submit" value="Send" />
       </form>
+      </div>
+     </div>
+     
   );
 };
 
