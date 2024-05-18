0944621af71354918df3776b38860b7a9f7f9df1
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-05-17 22:14:14 -0400

    contact form is working, i installes a emailjs package that will send email from a user to my acc

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,52 +1,106 @@
-import React from "react";
-import github from "../../gif-files/Github.json";
-import linkedin from "../../gif-files/LinkedIn.json"
-import email from "../../gif-files/Email.json"
-import Lottie from "lottie-react";
-import "./Contact.css";
+// import React from "react";
+// import github from "../../gif-files/Github.json";
+// import linkedin from "../../gif-files/LinkedIn.json";
+// import email from "../../gif-files/Email.json";
+// import topbar from "../../gif-files/Top.json";
+// import Lottie from "lottie-react";
+// import "./Contact.css";
 
-function Contact() {
-    return (
-        <div className="contact-container">
+// function Contact() {
+//   return (
+//     <div className="contact-container">
+//       <h2 className="contact-titile">
+//         <span>Contact Me</span>
+//       </h2>
+//       <div className="contact-main">
+//         <h3 className="con-class">Keep In Touch!</h3>
 
-            <h2 className="contact-titile">
-                <span>Contact Me</span>
-            </h2>
+//         <p className="con-msg">
+//           DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
+//         </p>
+//         <i>New York, NY. USA</i>
+//         <div className="con-item-div">
+//           <ul className="con-items">
+//             <li>
+//               <a href="https://github.com/arehmanlatif1" className="social-btns">
+//                 <Lottie
+//                   animationData={github}
+//                   loop={true}
+//                   style={{ width: "4rem" }}
+//                 />
+//                 Github
+//               </a>
+//             </li>
+//             <li>
+//               <a href="https://www.linkedin.com/in/abdulrehmanlatif1/" className="social-btns">
+//                 <Lottie
+//                   animationData={linkedin}
+//                   loop={true}
+//                   style={{ width: "5rem" }}
+//                 />
+//                 LinkedIn
+//               </a>
+//             </li>
+//             <li>
+//               <a href="mailto:arehmanlatif@gmail.com" className="social-btns">
+//                 <Lottie
+//                   animationData={email}
+//                   loop={true}
+//                   style={{ width: "7rem" }}
+//                 />
+//                 Email
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Contact;
 
-            <div className="contact-main">
-                
-                <h3 className="con-class">Keep In Touch!</h3>
-                <p className="con-msg">DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.</p>
-                <i>New York, NY. USA</i>
-                <div className="con-item-div">
-                <ul className="con-items">
-                    <li>
-                        <a href="https://github.com/arehmanlatif1">
-                    <Lottie animationData={github} loop={true}  style={{ width: "5rem" }} />
-                        Github
-                        </a>
-                        </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/abdulrehmanlatif1/">
-                    <Lottie animationData={linkedin} loop={true}  style={{ width: "5rem" }} />
-                        LinkedIn
-                        </a>
-                        </li>
-                    <li>
-                        <a email="arehmanlatif@gmail.com">
-                    <Lottie animationData={email} loop={true}  style={{ width: "7rem" }} />
-                        Email
-                        </a>
-                        </li>
-                </ul>
-                </div>
-                <hr />
 
-            </div>
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-           
-        </div>
 
-    )
-}
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_p50z9gc",
+        "template_c3yy5w8",
+        form.current,
+        "rsl9wvoTbM1uO250C"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+  );
+};
+
 export default Contact;
+
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import backgb from "../../gif-files/Bottom.json"
-import Lottie from 'lottie-react'
-import "./Footer.css"
+import React from 'react';
+import './Footer.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 function Footer() {
   return (
-    <div className='footer-container'>
-        <div className="lottie-container">
-            <Lottie animationData={backgb} loop={true} />
-            </div>
-    </div>
-  )
+    <footer>
+      <div className="footerContainer">
+        <div className="socialIcons">
+          <a href="https://www.linkedin.com/in/abdulrehmanlatif1/"><i className="fa-brands fa-linkedin"></i></a>
+          <a href="https://github.com/arehmanlatif1"><i className="fa-brands fa-github"></i></a>
+          <a href="#"><i className="fa-brands fa-facebook"></i></a>
+          <a href="#"><i className="fa-brands fa-instagram"></i></a>
+          <a href="#"><i className="fa-brands fa-twitter"></i></a>
+
+        </div>
+        <div className="footerNav">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#project">Project</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footerBottom">
+        <p>Copyright &copy;2023; Designed by <span className="designer">Abdul</span></p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
