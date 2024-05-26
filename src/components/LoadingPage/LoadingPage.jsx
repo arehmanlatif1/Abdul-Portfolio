1fc28913cb76fac36d6bd6ef8685a64f34b23774
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-05-25 23:33:22 -0400

    kweneikf

--- src/components/LoadingPage/LoadingPage.jsx
+++ src/components/LoadingPage/LoadingPage.jsx
@@ -7,7 +7,16 @@ const LoadingPage = () => {
   return (
     <div className="loading-container">
       <Lottie animationData={loadingAnimation} loop={true} style={{ width: '30rem' }} />
-      <p className="loading-text">Loading<span className="typing-animation">...</span></p>
+      <p className="loading-text">
+        <span className="typing-animation">L</span>
+        <span className="typing-animation">O</span>
+        <span className="typing-animation">A</span>
+        <span className="typing-animation">D</span>
+        <span className="typing-animation">I</span>
+        <span className="typing-animation">N</span>
+        <span className="typing-animation">G</span>
+        <span className="typing-animation">...</span>
+        </p>
       <h2 className='loading-title'>Abdul Rehman</h2>
     </div>
   );
